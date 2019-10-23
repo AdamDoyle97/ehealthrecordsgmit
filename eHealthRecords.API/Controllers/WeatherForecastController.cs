53527cd3d9ed7b0d0666e01c7a141316d324899f
Adam Doyle <doyleadam3@gmail.com>
2019-10-23 23:09:04 +0100

    Initial Commit

---
+++ eHealthRecords.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eHealthRecords.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace eHealthRecords.API.Controllers
+{
+    //http//localhost:5000/api (default it goes to port 5000)
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public WeatherForecastController(DataContext context) 
+        {
+            _context = context;
+
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeatherForecast() //async code is used for when more than one user is requesting
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+    }
+}
---
+++ eHealthRecords.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using eHealthRecords.API.Models;
+using Microsoft.EntityFrameworkCore; //had to install from --> dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 3.0.0  
+
+namespace eHealthRecords.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Value> Values { get; set; } //this will be the table name in sql database
+
+    }
+}
