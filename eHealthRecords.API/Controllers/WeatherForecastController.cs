e71efce36b149c16fa2d254109e767fe960d8717
Adam Doyle <doyleadam3@gmail.com>
2019-10-30 14:25:47 +0000

    new

--- eHealthRecords.API/Controllers/WeatherForecastController.cs
+++ eHealthRecords.API/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using eHealthRecords.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Logging;
 namespace eHealthRecords.API.Controllers
 {
     //http//localhost:5000/api (default it goes to port 5000)
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
