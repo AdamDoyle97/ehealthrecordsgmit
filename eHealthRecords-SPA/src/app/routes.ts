27a1e714ed3a0f96c11f79fb980fc211ea90a3a5
Adam Doyle <doyleadam3@gmail.com>
2019-11-17 15:44:45 +0000

    routing

--- eHealthRecords-SPA/src/app/routes.ts
+++ eHealthRecords-SPA/src/app/routes.ts
@@ -4,11 +4,18 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { MessageBundle } from '@angular/compiler';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'members', component: MemberListComponent},
-    { path: 'messages', component: MessagesComponent},
-    { path: 'lists', component: ListsComponent},
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
